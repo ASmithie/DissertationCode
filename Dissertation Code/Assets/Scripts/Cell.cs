2047a50ebd87facac64f02a6ded9ecbfdcaeb94d
ASmithie <alecsmith_@outlook.com>
2022-02-03 19:00:52 +0000

    Grids have a material

--- Dissertation Code/Assets/Scripts/Cell.cs
+++ Dissertation Code/Assets/Scripts/Cell.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
 public class Cell : MonoBehaviour
 {
 
     public float airPressure;
     public float heat;
 
-    public Vector3[] corners;
+    Vector3[] corners;
 
-
-    // Start is called before the first frame update
     void OnDrawGizmos()
     {
-        corners = new Vector3[8];
         MakeCube();
 
         Gizmos.matrix = transform.localToWorldMatrix;
@@ -25,21 +25,48 @@ public class Cell : MonoBehaviour
         }
     }
 
-    void MakeCube()
+    // Start is called before the first frame update
+    void Start()
     {
-        corners[0] = new Vector3(0, 0, 0);
-        corners[1] = new Vector3(1, 0, 0);
-        corners[2] = new Vector3(0, 0, 1);
-        corners[3] = new Vector3(1, 0, 1);
-        corners[4] = new Vector3(0, 1, 0);
-        corners[5] = new Vector3(1, 1, 0);
-        corners[6] = new Vector3(0, 1, 1);
-        corners[7] = new Vector3(1, 1, 1);
+        //RenderCube();
     }
 
-    // Update is called once per frame
-    void Update()
+    void MakeCube()
     {
-        
+        corners = new Vector3[8];
+        corners[0] = new Vector3(0, 0, 0);
+        corners[1] = new Vector3(1, 0, 0);
+        corners[2] = new Vector3(1, 1, 0);
+        corners[3] = new Vector3(0, 1, 0);
+        corners[4] = new Vector3(0, 1, 1);
+        corners[5] = new Vector3(1, 1, 1);
+        corners[6] = new Vector3(1, 0, 1);
+        corners[7] = new Vector3(0, 0, 1);
     }
+
+    void RenderCube()
+    {
+        int[] triangles = {
+            0, 2, 1, //face front
+	        0, 3, 2,
+            2, 3, 4, //face top
+	        2, 4, 5,
+            1, 2, 5, //face right
+	        1, 5, 6,
+            0, 7, 4, //face left
+	        0, 4, 3,
+            5, 4, 7, //face back
+	        5, 7, 6,
+            0, 6, 7, //face bottom
+	        0, 1, 6
+        };
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+        mesh.vertices = corners;
+        mesh.triangles = triangles;
+        mesh.Optimize();
+        mesh.RecalculateNormals();
+    }
+
 }
