65435fe14ea9758829ca848c2b87877462895871
ASmithie <alecsmith_@outlook.com>
2022-02-03 22:06:38 +0000

    CA

---
+++ Dissertation Code/Assets/Scripts/CellularAutomata.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellularAutomata : MonoBehaviour
+{
+
+    public Grid grid;
+
+    public Cell[] cells;
+    public Cell[] newCells;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cells = grid.cells;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            cells
+        }
+    }
+}
